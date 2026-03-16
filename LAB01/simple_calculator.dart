a7e2b6d8b0972fb523d1a5c2af37d4873c0b6735
timmy <hapsarinajwarahma@gmail.com>
2026-03-16 08:21:18 +0700

    Create simple_calculator.dart

---
+++ LAB01/simple_calculator.dart
@@ -0,0 +1,15 @@
+void main(){
+  double angka1 = 10;
+  double angka2 = 5;
+  
+  print("simple calculator");
+  print("Angka: $angka1 dan $angka2");
+  
+  print("Penjumlahan (+): ${angka1 + angka2}");
+  
+  print("Pengurangan (-): ${angka1 - angka2}");
+  
+  print("Perkalian (*): ${angka1 * angka2}");
+  
+  print("Pembagian (/): ${angka1 / angka2}");
+}
