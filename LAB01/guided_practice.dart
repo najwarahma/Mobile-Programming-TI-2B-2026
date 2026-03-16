4f7405940be81b45f07cb34e944e4d1c4aa46aa8
timmy <hapsarinajwarahma@gmail.com>
2026-03-16 08:26:19 +0700

    Create guided_practice.dart

---
+++ LAB01/guided_practice.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Najwa Rahma";
+  String studentId = "4.33.24.1.18";
+  String major = "Teknologi Rekayasa Jaringan";
+
+  print("Name: $name");
+  print("Student ID: $studentId");
+  print("Major: $major");
+}
